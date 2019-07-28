4b763218d87da9354e7672cfbdac87b6b8c339fb
incontumaciam <incontumaciam@gmail.com>
2019-07-28 07:19:03 +0300

    added methods: setDataCharacteristics, setFlowControl, setBaudRate

--- index.js
+++ index.js
@@ -294,6 +294,7 @@ const FT_FLOW_CONTROL = {
 Object.freeze(FT_FLOW_CONTROL);
 
 class FTDI {
+
     /**
      * Class wrapper for FTD2XX.DLL
      * @constructor
@@ -302,15 +303,40 @@ class FTDI {
         this._ftHandle = null;
     }
 
+    _checkFtHandle(runIfOk) {
+        let ftStatus = FT_STATUS.FT_OTHER_ERROR;
+        if(this._ftHandle != null && this._ftHandle !== 0) {
+            ftStatus = callback();
+        }
+        return ftStatus;
+    }
+
+    _setUpFtdiDevice() {
+        // Initialise port data characteristics
+        let wordLength = FT_DATA_BITS.FT_BITS_8;
+        let stopBits = FT_STOP_BITS.FT_STOP_BITS_1;
+        let parity = FT_PARITY.FT_PARITY_NONE;
+        ftStatus = _ftdi.setDataCharacteristics(this._ftHandle, wordLength, stopBits, parity);
+        if(ftStatus !== FT_STATUS.FT_OK) return ftStatus;
+        let flowControl = FT_FLOW_CONTROL.FT_FLOW_NONE;
+        let xon = 0x11;
+        let xoff = 0x13;
+        ftStatus = _ftdi.setFlowControl(this._ftHandle, flowControl, xon, xoff);
+        if(ftStatus !== FT_STATUS.FT_OK) return ftStatus;
+        // Initialise Baud rate
+        let baudRate = 9600;
+        return _ftdi.setBaudRate(this._ftHandle, baudRate);
+    }
+
     /**
      * @typedef GetNumberOfDevicesResult
      * @type {Object}
-     * @property {Number} ftStatus - value from FT_CreateDeviceInfoList
-     * @property {Number} devCount - the number of FTDI devices available
+     * @property {Number} ftStatus Value from FT_CreateDeviceInfoList
+     * @property {Number} devCount The number of FTDI devices available
      */
     /**
      * Gets the number of FTDI devices available
-     * @return {GetNumberOfDevicesResult}
+     * @returns {GetNumberOfDevicesResult}
      */
     static getNumberOfDevices() {
         return _ftdi.createDeviceInfoList();
@@ -326,23 +352,23 @@ class FTDI {
      * Type that holds device information for GetDeviceInformation method
      * @typedef DeviceInfo
      * @type {Object}
-     * @property {Number} flags - Indicates device state. Can be any combination of the following: FT_FLAGS.FT_FLAGS_OPENED, FT_FLAGS.FT_FLAGS_HISPEED
-     * @property {Number} type - Indicates the device type. Can be one of the following: FT_DEVICE_232R, FT_DEVICE_2232C, FT_DEVICE_BM, FT_DEVICE_AM, FT_DEVICE_100AX or FT_DEVICE_UNKNOWN
-     * @property {Number} id - The Vendor ID and Product ID of the device
-     * @property {Number} locId - The physical location identifier of the device
-     * @property {Number} serialNumber - The device serial number
-     * @property {Number} description - The device description
-     * @property {FtHandle} ftHandle - This value is not used externally and is provided for information only. If the device is not open, ftHandle.value is 0.
+     * @property {Number} flags Indicates device state. Can be any combination of the following: FT_FLAGS.FT_FLAGS_OPENED, FT_FLAGS.FT_FLAGS_HISPEED
+     * @property {Number} type Indicates the device type. Can be one of the following: FT_DEVICE_232R, FT_DEVICE_2232C, FT_DEVICE_BM, FT_DEVICE_AM, FT_DEVICE_100AX or FT_DEVICE_UNKNOWN
+     * @property {Number} id The Vendor ID and Product ID of the device
+     * @property {Number} locId The physical location identifier of the device
+     * @property {Number} serialNumber The device serial number
+     * @property {Number} description The device description
+     * @property {FtHandle} ftHandle This value is not used externally and is provided for information only. If the device is not open, ftHandle.value is 0.
      */
     /**
      * @typedef GetDeviceListResult
      * @type {Object}
-     * @property {Number} ftStatus - value from FT_GetDeviceInfoDetail
+     * @property {Number} ftStatus Value from FT_GetDeviceInfoDetail
      * @property {Array.<DeviceInfo>} deviceInfoList
      */
     /**
      * Gets information on all of the FTDI devices available
-     * @return { GetDeviceListResult} result.ftStatus - value from FT_CreateDeviceInfoList,
+     * @returns {GetDeviceListResult}
      */
     static getDeviceList() {
         let deviceInfoList = new Array();
@@ -356,38 +382,65 @@ class FTDI {
     /**
      * Opens the FTDI device with the specified index,
      * initialises the device to 8 data bits, 1 stop bit, no parity, no flow control and 9600 Baud
-     * @param {Number} index - Index of the device to open,
+     * @param {Number} index Index of the device to open,
      * note that this cannot be guaranteed to open a specific device
-     * @return {Number} ftStatus - Status values for FTDI devices
+     * @returns {Number} ftStatus Status values for FTDI device
      */
     openByIndex(index) {
         let { ftStatus, ftHandle } = _ftdi.open(index);
         if(ftStatus === FT_STATUS.FT_OK || ftHandle.value !== 0) {
             this._ftHandle = ftHandle;
-            // Initialise port data characteristics
-            let wordLength = FT_DATA_BITS.FT_BITS_8;
-            let stopBits = FT_STOP_BITS.FT_STOP_BITS_1;
-            let parity = FT_PARITY.FT_PARITY_NONE;
-            ftStatus = _ftdi.setDataCharacteristics(ftHandle, wordLength, stopBits, parity);
-            let flowControl = FT_FLOW_CONTROL.FT_FLOW_NONE;
-            let xon = 0x11;
-            let xoff = 0x13;
-            ftStatus = _ftdi.setFlowControl(ftHandle, flowControl, xon, xoff);
-            // Initialise Baud rate
-            let baudRate = 9600;
-            ftStatus = _ftdi.setBaudRate(ftHandle, baudRate);
+            this._setUpFtdiDevice();
         } else {
             this._ftHandle = null;
         }
         return ftStatus;
     }
+
+    /**
+     * Sets the data bits, stop bits and parity for the device
+     * @param {Number} wordLength The number of data bits for UART data. Valid values are FT_DATA_BITS.FT_DATA_7 or FT_DATA_BITS.FT_BITS_8
+     * @param {Number} stopBits The number of stop bits for UART data. Valid values are FT_STOP_BITS.FT_STOP_BITS_1 or FT_STOP_BITS.FT_STOP_BITS_2
+     * @param {Number} parity The parity of the UART data. Valid values are FT_PARITY.FT_PARITY_NONE, FT_PARITY.FT_PARITY_ODD, FT_PARITY.FT_PARITY_EVEN, FT_PARITY.FT_PARITY_MARK or FT_PARITY.FT_PARITY_SPACE
+     * @returns {Number} ftStatus ftStatus value from FT_SetDataCharacteristics
+     */
+    setDataCharacteristics(wordLength, stopBits, parity) {
+        return this._checkFtHandle(() => _ftdi.setDataCharacteristics(this._ftHandle, wordLength, stopBits, parity));
+    }
+
+    /**
+     * Sets the flow control type.
+     * @param {Number} flowControl The type of flow control for the UART. Valid values are FT_FLOW_CONTROL.FT_FLOW_NONE, FT_FLOW_CONTROL.FT_FLOW_RTS_CTS, FT_FLOW_CONTROL.FT_FLOW_DTR_DSR or FT_FLOW_CONTROL.FT_FLOW_XON_XOFF
+     * @param {Number} xon The Xon character for Xon/Xoff flow control. Ignored if not using Xon/XOff flow control
+     * @param {Number} xoff The Xoff character for Xon/Xoff flow control. Ignored if not using Xon/XOff flow control
+     * @returns {Number} ftStatus Value from FT_SetFlowControl
+     */
+    setFlowControl(flowControl, xon, xoff) {
+        return this._checkFtHandle(() => _ftdi.setFlowControl(flowControl, xon, xoff));
+    }
+
+    // Intellisense comments
+        /// <summary>
+        /// Sets the current Baud rate.
+        /// </summary>
+        /// <returns>FT_STATUS value from FT_SetBaudRate in FTD2XX.DLL</returns>
+        /// <param name="BaudRate">The desired Baud rate for the device.</param>
+    /**
+     * Sets the current Baud rate.
+     * @param {Number} baudRate The desired Baud rate for the device
+     * @retern {Number} ftStatus Value from FT_SetBaudRate
+     */
+    setBaudRate(baudRate) {
+        return this._checkFtHandle(() => _ftdi.setBaudRate(baudRate));
+    }
 }
-
-
 
 module.exports = {
     FT_STATUS: FT_STATUS,
     FT_DEVICE: FT_DEVICE,
     FT_FLAGS: FT_FLAGS,
+    FT_DATA_BITS: FT_DATA_BITS,
+    FT_STOP_BITS: FT_STOP_BITS,
+    FT_PARITY: FT_PARITY,
     FTDI: FTDI
 }
--- test.js
+++ test.js
@@ -12,5 +12,5 @@ console.log(Object.keys(FTD2XX.FT_STATUS)[ftStatus]);
 console.log(FTD2XX.FTDI.getDeviceList());
 console.log("!!!");
 console.log(FTD2XX.FTDI.getDeviceList().deviceInfoList[0]);
-console.log(ftdi.openByIndex(0));
+console.log(Object.keys(FTD2XX.FT_STATUS)[ftdi.openByIndex(0)]);
 console.log(FTD2XX.FTDI.getDeviceList().deviceInfoList[0]);
